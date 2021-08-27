73de7616fdbf67ad864a7fe862bfc72ba7cced35
Thu Nguyen <73574530+AnhThuNg@users.noreply.github.com>
2021-08-27 17:12:46 +0700

    Update BookApplication.java

--- src/book/BookApplication.java
+++ src/book/BookApplication.java
@@ -6,10 +6,10 @@ import java.util.List;
 
 public class BookApplication {
     public static void main(String[] args) throws ClassNotFoundException {
-        String db_conn = "jdbc:sqlserver://DESKTOP-FORO49H\\SQLEXPRESS:1433;"
+        String db_conn = "jdbc:sqlserver:localhost:1433;"
                 + "databaseName=BookService;"
-                + "user=sa;"
-                + "password=sa2021;"
+                + "user=***;"
+                + "password=***;"
                 + "integratedSecurity=true;";
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
         try {
@@ -17,41 +17,48 @@ public class BookApplication {
             Statement statement = connection.createStatement();
             BookServiceImpl bookService = new BookServiceImpl();
             //TODO: Find by Id
-//            Book book = bookService.findBookById(1, statement);
-//            System.out.println(book);
+            Book book = bookService.findBookById(1, statement);
+            System.out.println(book);
+            
             //TODO: Find all
-//            List<Book> bookList = bookService.findAll(statement);
-//            bookList.forEach(System.out::println);
+            List<Book> bookList = bookService.findAll(statement);
+            bookList.forEach(System.out::println);
+            
             //TODO: Delete Book By Id
-//            bookService.deleteBookById(9, statement);
-//            List<Book> bookList = bookService.findAll(statement);
-//            bookList.forEach(System.out::println);
+            bookService.deleteBookById(9, statement);
+            List<Book> bookList = bookService.findAll(statement);
+            bookList.forEach(System.out::println);
+            
             //TODO: Insert Book By Id
-//            Book book = new Book();
-//            book.setId(9);
-//            book.setTitle("cdb");
-//            bookService.insertBook(book, statement);
-//            List<Book> bookList = bookService.findAll(statement);
-//            bookList.forEach(System.out::println);
+            Book book = new Book();
+            book.setId(9);
+            book.setTitle("book-name");
+            bookService.insertBook(book, statement);
+            List<Book> bookList = bookService.findAll(statement);
+            bookList.forEach(System.out::println);
+            
             //TODO: Update Book by Id
-//            Book book1 = new Book();
-//            book1.setId(8);
-//            book1.setTitle("capbo");
-//            bookService.updateBook(book1, statement);
-//            List<Book> bookList = bookService.findAll(statement);
-//            bookList.forEach(System.out::println);
+            Book book1 = new Book();
+            book1.setId(8);
+            book1.setTitle("book-name");
+            bookService.updateBook(book1, statement);
+            List<Book> bookList = bookService.findAll(statement);
+            bookList.forEach(System.out::println);
+            
             //TODO: Find By Title
-//            List<Book> bookList1 = bookService.searchBookByName("bo tran", statement);
-//            bookList1.forEach(System.out::println);
+            List<Book> bookList1 = bookService.searchBookByName("book-name", statement);
+            bookList1.forEach(System.out::println);
+            
             //TODO: Delete Book By Name
-//            List<Book> bookList = bookService.deleteBookByName("shin cau be but chi", statement);
-//            bookList.forEach(System.out::println);
+            List<Book> bookList = bookService.deleteBookByName("book-name", statement);
+            bookList.forEach(System.out::println);
+            
             //TODO: Find multi rows by Id
             List<Integer> ids = new ArrayList<>();
             ids.add(1);
             ids.add(2);
             ids.add(4);
-            // [1, 2, 4]
+            //input: [1, 2, 4]
             List<Book> bookList = bookService.findBookByListId(ids, statement);
             bookList.forEach(System.out::println);
 
